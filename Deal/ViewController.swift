05de190d7242942fef88412b0d54306711d78a10
ben <bensadiku65@gmail.com>
2019-08-09 14:51:30 -0700

    Initial commit

---
+++ Deal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Deal
+//
+//  Created by ben on 8/9/19.
+//  Copyright © 2019 ben. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
